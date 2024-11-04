b39c09302e59af226bf0739057dc5ad5cfc3b12d
Marius Storhaug <marstor@hotmail.com>
2024-11-04 10:55:33 +0100

    🌟 [Breaking change]: Initial version (#1)
    * Initial version
    
    * Fix test
    
    * Fix test
    
    * Add tests
    
    * Fix tests
    
    * TEst
    
    * test
    
    * Test
    
    * Added docs
    
    * Fix
    
    * Done?

--- README.md
+++ README.md
@@ -1,17 +1,44 @@
-# Template-action
+# Get-AppJWT
 
-A template repository for GitHub Actions
+This action gets a JWT token for a GitHub Application.
 
 ## Usage
 
 ### Inputs
 
-### Secrets
+| Input | Required | Description |
+| ----- | -------- | ----------- |
+| `ClientID` | Yes | The Client ID of the GitHub App |
+| `PrivateKey` | Yes | The Private Key of the GitHub App |
 
 ### Outputs
 
-### Example
+| Output | Description |
+| ------ | ----------- |
+| `JWT` | The JWT for the GitHub App |
+
+### Example: Using the JWT to access the GitHub API
 
 ```yaml
-Example here
+<...>
+  - name: Get-AppJWT
+    id: Get-AppJWT
+    uses: ./
+    with:
+      ClientID: ${{ secrets.CLIENT_ID }}
+      PrivateKey: ${{ secrets.PRIVATE_KEY }}
+
+  - name: Get app info
+    shell: pwsh
+    env:
+      JWT: ${{ steps.Action-Test.outputs.JWT }}
+    run: |
+      # Access the GitHub /app endpoint using the JWT as bearer token
+      Invoke-RestMethod -Uri "$env:GITHUB_API_URL/app" -Method Get -Headers @{
+          Authorization = "Bearer $env:JWT"
+      }
 ```
+
+## Links
+
+- [Generating a JSON Web Token (JWT) for a GitHub App | GitHub Docs](https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app)
--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,7 +1,47 @@
 [CmdletBinding()]
-param(
+param (
     [Parameter()]
-    [string] $Subject = $env:GITHUB_ACTION_INPUT_subject
+    [string] $ClientID = $env:GITHUB_ACTION_INPUT_ClientID,
+
+    [Parameter()]
+    [string] $PrivateKey = $env:GITHUB_ACTION_INPUT_PrivateKey
 )
 
-Write-Host "Hello, $subject!"
+$header = [Convert]::ToBase64String(
+    [System.Text.Encoding]::UTF8.GetBytes(
+        (
+            ConvertTo-Json -InputObject @{
+                alg = 'RS256'
+                typ = 'JWT'
+            }
+        )
+    )
+).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+
+$payload = [Convert]::ToBase64String(
+    [System.Text.Encoding]::UTF8.GetBytes(
+        (
+            ConvertTo-Json -InputObject @{
+                iat = [System.DateTimeOffset]::UtcNow.AddSeconds(-10).ToUnixTimeSeconds()
+                exp = [System.DateTimeOffset]::UtcNow.AddMinutes(10).ToUnixTimeSeconds()
+                iss = $ClientID
+            }
+        )
+    )
+).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+
+$rsa = [System.Security.Cryptography.RSA]::Create()
+$rsa.ImportFromPem($PrivateKey)
+
+$signature = [Convert]::ToBase64String(
+    $rsa.SignData(
+        [System.Text.Encoding]::UTF8.GetBytes("$header.$payload"),
+        [System.Security.Cryptography.HashAlgorithmName]::SHA256,
+        [System.Security.Cryptography.RSASignaturePadding]::Pkcs1
+    )
+).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+
+$jwt = "$header.$payload.$signature"
+Write-Host "::add-mask::$jwt"
+Write-Host "JWT: $jwt"
+"JWT=$jwt" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
